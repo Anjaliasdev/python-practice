482b212b5ad85814f7f067d88af84ea89cdf958e
Anjaliasdev <anjalisen14324@gmail.com>
2026-09-13 11:42:21 +0530

    Added more practice

---
+++ inputs.py
@@ -0,0 +1,16 @@
+# input function
+
+
+#a= int(input("Enter number"))
+#b= int(input("Enter number"))
+#print("Number a is= ", a)
+#print("Number b is= ", b)
+
+a = int(input("Enter number"))
+b = int(input("Enter number"))
+
+print("Number a is = ", a)
+print("Number b is = ", b)
+print("Sum is ", a+b)
+
+
---
+++ variables and datatype.py
@@ -0,0 +1,24 @@
+#Q1. write a python program to add two number.
+
+#a = 5
+#b = 6
+
+#print("Sum of a and b = ", a+b)
+
+#Q2. Write a python program to find remainder when a number is divided by z.
+
+#a = 34
+#b = 6
+
+#print("Remainder of a and b = ", a%b)
+
+#Q3 check the type of variable assigned using input() function.
+
+#a = int(input("Enter the value of a : "))
+
+#print(type(a))
+
+#Q4. Use comparison operator to find out wether a given variable a is greater than'b' or not.
+
+a = int(input("Enter no. 1"))
+
